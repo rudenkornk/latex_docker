c7240b32dd8a50a5dff6aa05b32adea0396e7914
Nikita Rudenko <rudenkornk@gmail.com>
2022-04-25 12:21:13 +0300

    Sort packages in install_support.sh

--- install_support.sh
+++ install_support.sh
@@ -5,8 +5,8 @@ set -x
 echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | debconf-set-selections
 apt-get update
 DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends \
-  make \
   file \
+  make \
   msttcorefonts \
   python3-pygments \
   ttf-mscorefonts-installer \
