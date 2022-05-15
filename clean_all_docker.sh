0320320a6793d2d3431e864dce6c3aef5582ba94
Nikita Rudenko <rudenkornk@gmail.com>
2022-05-15 18:44:57 +0300

    Amend clean_all_docker.sh script

--- clean_all_docker.sh
+++ clean_all_docker.sh
@@ -3,7 +3,10 @@
 # Helper script to remove all docker containers, volumes and dangling images
 # Use it with caution since it removes ALL containers
 
-set -x
+set -o errexit
+set -o pipefail
+set -o nounset
+#set -o xtrace
 
 docker container ls --quiet | ifne xargs docker stop
 docker container ls --quiet -all | ifne xargs docker rm
