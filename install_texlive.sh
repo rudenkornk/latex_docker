7a4a1b8e6648f9539a1dc53ee2982bd9e4f6b883
Nikita Rudenko <rudenkornk@gmail.com>
2022-07-30 00:54:52 +0300

    Pin texlive version

--- install_texlive.sh
+++ install_texlive.sh
@@ -3,5 +3,5 @@
 set -x
 
 apt-get update
-DEBIAN_FRONTEND=noninteractive TZ=Etc/UTC apt-get install -y --no-install-recommends texlive-full
-
+DEBIAN_FRONTEND=noninteractive TZ=Etc/UTC apt-get install -y --no-install-recommends \
+  texlive-full=2021.20220204-1
