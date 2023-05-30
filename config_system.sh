2d9bcd05b5190750cfb0a3577d805b2ff0dbf6d9
Nikita Rudenko <nikita.rudenko@syntacore.com>
2023-05-30 11:50:27 +0300

    Add nodejs package

--- config_system.sh
+++ config_system.sh
@@ -5,6 +5,18 @@ set -o pipefail
 set -o nounset
 #set -o xtrace
 
+DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends \
+  curl \
+  lsb-release \
+
+
+DISTRO=$(lsb_release --codename --short)
+ARCH=$(dpkg --print-architecture)
+PUBKEY=/etc/apt/trusted.gpg.d/nodejs.asc
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource.gpg.key > $PUBKEY
+echo "deb [arch=$ARCH signed-by=$PUBKEY] https://deb.nodesource.com/node_19.x $DISTRO main" > /etc/apt/sources.list.d/nodejs.list
+
+
 echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | debconf-set-selections
 apt-get update
 DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends \
@@ -14,6 +26,7 @@ DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends \
   libc6-dev `# for latexindent` \
   make \
   msttcorefonts \
+  nodejs `# for prettier formatter` \
   poppler-utils `# for pdfinfo tests` \
   python3-pip \
   python3-pygments `# for minted latex package` \
